7f576a8a534c6a4f1bda41ccf78bbf5553f49cca
apple <apple@ele.cash>
2019-08-30 15:18:49 +0800

    no message

---
+++ CBModuleA/CBModuleA/BViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BViewController.swift
+//  CBModuleA
+//
+//  Created by apple on 2019/8/30.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+public class BViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
